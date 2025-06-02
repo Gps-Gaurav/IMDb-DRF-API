5f5d67641df29f9e4e6102d6867e20a46d17c42e
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-06-02 12:01:23 +0530

    cc

--- imdb/imdb_api/urls.py
+++ imdb/imdb_api/urls.py
@@ -1,10 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('list/', views.movie_list, name='movie_list'),
     path('list/<int:pk>/', views.movie_detail, name='movie_detail'),
-    path('', include('imdb_api.urls')),
 ]
